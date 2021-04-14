1b0e6492c68355a8dc4ae7ad19dc81b7824f98e5
AlexNazaroff <ale.krotov@gmail.com>
2021-04-14 14:45:52 +0500

    4 className for video logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
-import {Navbar} from "react-bootstrap";
+import {Navbar, NavbarBrand} from "react-bootstrap";
 
 class Header extends Component {
     render() {
         return (
-         <Navbar >
+            <Navbar>
+                <Container>
+                    <Nanvbar.brand href='/'>
+                        <img src=""
+                             height='30'
+                             width='30'
+                             className='d-inline-block align-top'
+                        />
+                    </Nanvbar.brand>
+                </Container>
 
-         </Navbar>
+            </Navbar>
         );
     }
 }
