9b500303a623adca5fa0495208d30d9c4706f803
AlexNazaroff <ale.krotov@gmail.com>
2021-04-14 14:29:46 +0500

    2 rcc Tab - класс

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Button}  from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+      <>
 <h1>text</h1>
+<Button variant='primary'>Button</Button>
+      </>
   );
 }
 
