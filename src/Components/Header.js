e77f767739a39573a56c3dddd6385e7bf7e5425c
AlexNazaroff <ale.krotov@gmail.com>
2021-04-14 14:32:33 +0500

    3 teg navbar import

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import {Navbar} from "react-bootstrap";
 
 class Header extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+         <Navbar >
+
+         </Navbar>
         );
     }
 }
