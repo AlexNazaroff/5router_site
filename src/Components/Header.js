39fd25f3ce342a6e6fb046f5af206f73d15054ec
AlexNazaroff <ale.krotov@gmail.com>
2021-04-14 18:04:54 +0500

    9 for scroll

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,39 +1,41 @@
 import React, {Component} from 'react';
-import {Navbar, Nav, Button, Form, FormControl, Container,  NavbarBrand} from "react-bootstrap";
+import {Navbar, Nav, Button, Form, FormControl, Container, NavbarBrand} from "react-bootstrap";
 import logo from './logo192.png'
 
 class Header extends Component {
     render() {
         return (
-            <Navbar collapseOnSelect expand='md' bg='dark' variant='dark'>
-                <Container>
-                    <NavbarBrand href='/'>
-                        <img src={logo}
-                             height='30'
-                             width='30'
-                             className='d-inline-block align-top'
-                             alt='logo'
-                        />
-                    </NavbarBrand>
-                    <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
-                    <Navbar.Collapse id='responsive-navbar-nav'>
-                        <Nav className='mr-auto'>
-                            <Nav.Link href='/'>Home </Nav.Link>
-                            <Nav.Link href='/about'>About us </Nav.Link>
-                            <Nav.Link href='/contacts'>Contacts </Nav.Link>
-                            <Nav.Link href='/blog'>Blog </Nav.Link>
-                        </Nav>
-                        <Form inline>
-                            <FormControl
-                                type='text'
-                                placeholder='Search'
-                                className='mr-sm-2'
+            <>
+                <Navbar collapseOnSelect expand='md' bg='dark' variant='dark'>
+                    <Container>
+                        <NavbarBrand href='/'>
+                            <img src={logo}
+                                 height='30'
+                                 width='30'
+                                 className='d-inline-block align-top'
+                                 alt='logo'
                             />
-                        <Button variant='outline-info'>Search</Button>
-                        </Form>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+                        </NavbarBrand>
+                        <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
+                        <Navbar.Collapse id='responsive-navbar-nav'>
+                            <Nav className='mr-auto'>
+                                <Nav.Link href='/'>Home </Nav.Link>
+                                <Nav.Link href='/about'>About us </Nav.Link>
+                                <Nav.Link href='/contacts'>Contacts </Nav.Link>
+                                <Nav.Link href='/blog'>Blog </Nav.Link>
+                            </Nav>
+                            <Form inline>
+                                <FormControl
+                                    type='text'
+                                    placeholder='Search'
+                                    className='mr-sm-2'
+                                />
+                                <Button variant='outline-info'>Search</Button>
+                            </Form>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </>
         );
     }
 }
